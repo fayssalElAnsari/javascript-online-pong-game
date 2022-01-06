4b8972374af2a561e0439c49a7948bfda91d3699
fayssalElAnsari <fayssal.el.ansari@gmail.com>
2022-01-06 23:26:32 +0100

    deleting

--- index.js
+++
@@ -1,8 +0,0 @@
-import http from 'http';
-import RequestController from './controller/requestController.js';
-
-const server = http.createServer(
-	(request, response) => new RequestController(request, response).handleRequest());
-
-server.listen(8080);
-
